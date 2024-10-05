4e0943b9bafcf6594e14d80e00b1a920e4950ad1
NTNET\dmw <deven.warang@beeswift.com>
2024-10-05 23:34:25 +0100

    Added new fonts

---
+++ src/Utils/GameFont.java
@@ -0,0 +1,51 @@
+package Utils;
+
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class GameFont {
+
+    private Font spiritFont;
+    private Font lemonTuesday;
+    private Font bananaYeti;
+    private Font sugarDeath2;
+
+    public GameFont() {
+        try {
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            InputStream stream1 = getClass().getResourceAsStream("/fonts/fightingSpiritTBS.ttf");
+            InputStream stream2 = getClass().getResourceAsStream("/fonts/lemonTuesday.otf");
+            InputStream stream3 = getClass().getResourceAsStream("/fonts/bananaYeti_Extrabold.ttf");
+            InputStream stream4 = getClass().getResourceAsStream("/fonts/sugarDeath2.ttf");
+
+            assert stream1 != null;
+            spiritFont = Font.createFont(Font.TRUETYPE_FONT, stream1).deriveFont(45f);
+            assert stream2 != null;
+            lemonTuesday = Font.createFont(Font.TRUETYPE_FONT, stream2).deriveFont(45f);
+            assert stream3 != null;
+            bananaYeti = Font.createFont(Font.TRUETYPE_FONT, stream3).deriveFont(45f);
+            assert stream4 != null;
+            sugarDeath2 = Font.createFont(Font.TRUETYPE_FONT, stream4).deriveFont(45f);
+            ge.registerFont(spiritFont);
+        } catch (FontFormatException | IOException | AssertionError exception) {
+            System.err.println("Fonts not loaded: " + exception);
+        }
+    }
+
+    public Font getFightingSpirit() {
+        return spiritFont;
+    }
+
+    public Font getLemonTuesday() {
+        return lemonTuesday;
+    }
+
+    public Font getBananaYeti() {
+        return bananaYeti;
+    }
+
+    public Font getSugarDeath2() {
+        return sugarDeath2;
+    }
+}
